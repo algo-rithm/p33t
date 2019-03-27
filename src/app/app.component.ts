d56d1e3fad0875c13fd5c428bff93f81546f5dab
Peter <balanesipf@gmail.com>
2019-03-27 09:13:14 -0400

    shoes on2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'p33t-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'p33 . Tee . coh';
+
+  constructor(private authService: AuthService ) {}
+
+  ngOnInit() {
+    this.authService.initAuthListener();
+  }
 }
